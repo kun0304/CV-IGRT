94e0de38c1d51ac3fd01c5b6bd42b85655a06832
Xiaokun Liang <xk.liang@qq.com>
2020-06-25 19:49:56 +0800

    Add files via upload

---
+++ densenet3d.py
@@ -0,0 +1,112 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+
+class Bottleneck(nn.Module):
+    def __init__(self, nChannels, growthRate):
+        super(Bottleneck, self).__init__()
+        interChannels = 4*growthRate
+        self.bn1 = nn.BatchNorm3d(nChannels)
+        self.conv1 = nn.Conv3d(nChannels, interChannels, kernel_size=1,
+                               bias=False)
+        self.bn2 = nn.BatchNorm3d(interChannels)
+        self.conv2 = nn.Conv3d(interChannels, growthRate, kernel_size=3,
+                               padding=1, bias=False)
+
+    def forward(self, x):
+        out = self.conv1(F.relu(self.bn1(x)))
+        out = self.conv2(F.relu(self.bn2(out)))
+        out = torch.cat((x, out), 1)
+        return out
+
+
+class SingleLayer(nn.Module):
+    def __init__(self, nChannels, growthRate):
+        super(SingleLayer, self).__init__()
+        self.bn1 = nn.BatchNorm3d(nChannels)
+        self.conv1 = nn.Conv3d(nChannels, growthRate, kernel_size=3,
+                               padding=1, bias=False)
+
+    def forward(self, x):
+        out = self.conv1(F.relu(self.bn1(x)))
+        out = torch.cat((x, out), 1)
+        return out
+
+
+class Transition(nn.Module):
+    def __init__(self, nChannels, nOutChannels):
+        super(Transition, self).__init__()
+        self.bn1 = nn.BatchNorm3d(nChannels)
+        self.conv1 = nn.Conv3d(nChannels, nOutChannels, kernel_size=1,
+                               bias=False)
+
+    def forward(self, x):
+        out = self.conv1(F.relu(self.bn1(x)))
+        out = F.avg_pool3d(out, 2)
+        return out
+
+
+class DenseNet3D(nn.Module):
+    def __init__(self, growthRate, depth, reduction, nClasses, bottleneck):
+        super(DenseNet3D, self).__init__()
+        nDenseBlocks = (depth-4) // 3
+        if bottleneck:
+            nDenseBlocks //= 2
+
+        nChannels = 2*growthRate
+        self.conv1 = nn.Conv3d(2, nChannels, kernel_size=3, padding=1,
+                               bias=False)
+        self.dense1 = self._make_dense(nChannels, growthRate, nDenseBlocks, bottleneck)
+        nChannels += nDenseBlocks*growthRate
+        nOutChannels = int(math.floor(nChannels*reduction))
+        self.trans1 = Transition(nChannels, nOutChannels)
+
+        nChannels = nOutChannels
+        self.dense2 = self._make_dense(nChannels, growthRate, nDenseBlocks, bottleneck)
+        nChannels += nDenseBlocks*growthRate
+        nOutChannels = int(math.floor(nChannels*reduction))
+        self.trans2 = Transition(nChannels, nOutChannels)
+
+        nChannels = nOutChannels
+        self.dense3 = self._make_dense(nChannels, growthRate, nDenseBlocks, bottleneck)
+        nChannels += nDenseBlocks*growthRate
+
+        self.bn1 = nn.BatchNorm3d(nChannels)
+        self.fc = nn.Linear(184, nClasses)
+        self.tanh = nn.Tanh()
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv3d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm3d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                m.bias.data.zero_()
+
+    def _make_dense(self, nChannels, growthRate, nDenseBlocks, bottleneck):
+        layers = []
+        for i in range(int(nDenseBlocks)):
+            if bottleneck:
+                layers.append(Bottleneck(nChannels, growthRate))
+            else:
+                layers.append(SingleLayer(nChannels, growthRate))
+            nChannels += growthRate
+        return nn.Sequential(*layers)
+
+    def forward(self, x, y):
+        x = torch.cat((x, y), 1)
+        x = torch.nn.functional.interpolate(x, size=[80, 80, 80], mode='trilinear')
+        out = self.conv1(x)
+        out = self.trans1(self.dense1(out))
+        out = self.trans2(self.dense2(out))
+        out = self.dense3(out)
+        out = torch.squeeze(F.avg_pool3d(F.relu(self.bn1(out)), 8))
+        out = out.reshape(x.size(0), -1)
+        out = self.fc(out)
+        out = self.tanh(out)
+        out = torch.mul(torch.tensor([10.0, 10.0, 10.0, 3.0, 3.0, 3.0]).repeat([out.shape[0], 1]).cuda(), out)
+        return out
---
+++ param.py
@@ -0,0 +1,37 @@
+# Coded by Xiaokun Liang
+# E-mail: xiaokun@qq.com
+# Stanford University
+
+
+# the index of training GPU
+gpu_index = '0'
+
+# the dir of the data
+training_data_path = './cv_dataset'
+
+# the dir of the saved model
+model_dir = './model_Result'
+
+# number of the training iteration
+num_iter = 100000
+
+# the initial learning rate
+lr = 0.01
+
+# the decay rate of the learning rate
+decay_rate = 0.1
+
+# decay times of the learning rate in training
+learning_rate_decay_times = 4
+
+# training batch size
+batch_size = 1
+
+# the weight of control volumes' ncc
+lambda_cv = 0.5
+
+# the size of the control volume (pixel)
+control_volume_size = (30, 40, 40)
+
+# save the model per # iteration
+save_net_nIter = 50
